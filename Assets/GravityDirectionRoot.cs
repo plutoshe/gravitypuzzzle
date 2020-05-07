a3de2c5e925f7f8799a4d63fb9a79d67f1bc5c37
plutoshe <plutoshe@gmail.com>
2020-05-07 05:15:46 -0600

    gravity change initialization

---
+++ Assets/GravityDirectionRoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public enum GravityDirection
+{
+    XPositive,
+    XNegtive,
+    YPositive,
+    YNegtive,
+    ZPositive,
+    ZNegtive,
+}
+
+public class GravityDirectionRoot : MonoBehaviour
+{
+    public GravityDirection m_GravityDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateRotationByGravityDirection(m_GravityDirection);
+    }
+    Vector3 UpdateRotationByGravityDirection(GravityDirection i_direction)
+    {
+        switch (i_direction)
+        {
+            case GravityDirection.XPositive: return new Vector3(1, 0, 0);
+            case GravityDirection.XNegtive: return new Vector3(-1, 0, 0);
+            case GravityDirection.YPositive: return new Vector3(0, 1, 0);
+            case GravityDirection.YNegtive: return new Vector3(0, -1, 0);
+            case GravityDirection.ZPositive: return new Vector3(0, 0, 1);
+            case GravityDirection.ZNegtive: return new Vector3(0, 0, -1);
+        }
+        return new Vector3(0, 0, 0);
+    }
+    // Update is called once per frame
+    void ChangeGravityDirection(GravityDirection i_direction)
+    {
+        m_GravityDirection = i_direction;
+        UpdateRotationByGravityDirection(m_GravityDirection);
+    }
+}
---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float m_NormalGravityAccleration = 9.8f;
+    private Rigidbody m_rigidbody;
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    Vector3 UpdateByGravity(float i_deltaTime)
+    {
+        return i_deltaTime * new Vector3(0, -m_NormalGravityAccleration, 0);
+    }
+
+    private void Update()
+    {
+        m_rigidbody.AddForce(UpdateByGravity(Time.deltaTime) * m_rigidbody.mass);
+    }
+}
